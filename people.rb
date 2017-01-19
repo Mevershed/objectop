bc3484f07ec0d038cd8939b3fce24d65d2dac5b2
mevershed <matthew@evershed.ca>
2017-01-19 14:18:13 -0500

    assignemnt 1

---
+++ people.rb
@@ -0,0 +1,23 @@
+class Person
+  def initialize(name)
+    @name = name
+
+  end
+  def greeting
+    "Hi, my name is #{@name}"
+
+  end
+end
+
+class Student < Person
+  def learn
+    "I get it!"
+  end
+
+end
+
+class Instructor < Person
+  def teach
+    "Everything in Ruby is an Object"
+  end
+end
